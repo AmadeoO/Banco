c76d2e5e52fbd3f036ab0b547dbf8f47c952567a
Gonzalo Mendoza <gonzalo.mendoza@globant.com>
2018-10-07 00:06:42 -0300

    Fixing errors

--- src/main/java/com/info/tp6/repositories/BancoRepository.java
+++ src/main/java/com/info/tp6/repositories/BancoRepository.java
@@ -10,10 +10,6 @@ import org.springframework.stereotype.Repository;
 import com.info.tp6.model.Banco;
 
 @Repository
-public interface BancoRepository extends CrudRepository<Banco,Long> {	
-	    
-	    public Banco findByNombre(String nombre);
-	    public List<Banco> findByAll();
-	    public Banco findById(String id_banco);
-
+public interface BancoRepository extends CrudRepository<Banco,String> {
+    public Banco findByNombre(String nombre);
 }
